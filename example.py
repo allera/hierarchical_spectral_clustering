6810f62d3b0aaf57f6bca37e709f39067a6e488a
allera <llera.a1@gmail.com>
2022-11-09 18:31:15 +0100

    Update example.py

--- example.py
+++ example.py
@@ -74,21 +74,8 @@ link_mat,minbic,labels = hc.spectral_clustering(yy,data)
 
 
 
-if 0: #cluster inside each cluster
 
-    #nested clustering
-    for nest in range(int(minbic)):
-        idx=np.ravel(np.argwhere(labels==nest))
-        nest_data=np.squeeze(data[idx[:,None],:])
-        nest_S2=S[idx[:,None],idx]
-        nest_S=hc.compute_similarity(X=nest_data,Similarity='euclid')
-        L,D,W=hc.compute_graph_Laplacian(S=nest_S)
-        yy,path_length=hc.compute_spectral_decomposition_graph_Laplacian(L=L,D=D,dim=1)
-        print('plotting results')
-        link_mat2,minbic2,labels2 = hc.spectral_clustering(yy,nest_data)  
-    
-    
-
+   
 usesklearn=0
 if usesklearn==1:
     from sklearn import cluster
